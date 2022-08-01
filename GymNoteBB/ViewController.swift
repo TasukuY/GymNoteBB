c1dd3fad850a01093429cbd1557c4722dccd902c
TasukuY <alohaty81@gmail.com>
2022-08-01 15:56:01 -0600

    Initial Commit

---
+++ GymNoteBB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GymNoteBB
+//
+//  Created by Tasuku Yamamoto on 8/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
